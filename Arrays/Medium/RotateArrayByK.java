5a20cd3b4c1e0a9d9e0555d98bec6742b55e5557
Ishita Jain <52002749+Ishita-22@users.noreply.github.com>
2026-02-24 18:02:58 +0530

    Create RotateArrayByK.java

---
+++ Arrays/Medium/RotateArrayByK.java
@@ -0,0 +1,53 @@
+/**
+ * Problem Name: Rotate Array by k Steps
+ * Difficulty: Medium
+ *
+ * Problem Statement:
+ * Rotate the array to the right by k steps.
+ *
+ * Pseudocode:
+ * 1. k = k % n
+ * 2. Reverse whole array
+ * 3. Reverse first k elements
+ * 4. Reverse remaining n-k elements
+ *
+ * Approach:
+ * Use reverse technique to rotate in-place.
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ *
+ * Example:
+ * Input: [1,2,3,4,5,6,7], k = 3
+ * Output: [5,6,7,1,2,3,4]
+ */
+
+import java.util.*;
+
+public class RotateArrayByK {
+
+    public static void rotate(int[] nums, int k) {
+
+        int n = nums.length;
+        k = k % n;
+
+        reverse(nums, 0, n-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, n-1);
+    }
+
+    private static void reverse(int[] nums, int start, int end) {
+
+        while(start < end) {
+            int temp = nums[start];
+            nums[start++] = nums[end];
+            nums[end--] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7};
+        rotate(nums,3);
+        System.out.println(Arrays.toString(nums));
+    }
+}
